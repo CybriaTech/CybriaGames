81bfb816051130816901509a1766569e4204d5a2
X-88 <timmytamle@icloud.com>
2024-07-11 22:17:24 -0500

    Initial commit

---
+++ scripts/importheader.js
@@ -0,0 +1,12 @@
+document.write(`
+    <header>
+        <img class="header-img" alt="CYBRIAGAMES" src="images/cybriagames/logo-name2half.png">
+        <nav class="header-links">
+            <a class="header-link" href="/">Home</a>
+            <a class="header-link" href="/games">Games</a>
+            <a class="header-link" href="/bookmarklets">Bookmarklets</a>
+            <a class="header-link" href="/widgetbot.html">Widgetbot</a>
+            <a class="header-link" href="/settings.html">Settings</a>
+        </nav>
+    </header>
+`);
