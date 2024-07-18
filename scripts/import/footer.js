57764f226c9e0e3626d6f3270bfa7548aede81a4
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-07-17 21:03:36 -0500

    Update footer.js

--- scripts/import/footer.js
+++ scripts/import/footer.js
@@ -1,10 +1,10 @@
-document.write("
+document.write('
     <footer>
         <p>&copy; ${new Date().getFullYear()} CybriaTech</p>
-        <nav class='footer-links'>
-            <a class='footer-link' href='/about.html'>About</a>
-            <a class='footer-link' href='/links.html'>Links</a>
-            <a class='footer-link' href='/socials.html'>Socials</a>
+        <nav class="footer-links">
+            <a class="footer-link" href="/about.html">About</a>
+            <a class="footer-link" href="/links.html">Links</a>
+            <a class="footer-link" href="/socials.html">Socials</a>
         </nav>
     </footer>
-");
+');
