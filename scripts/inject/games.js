fb45b561e85960b0b8538e3ee8bcf045c6fdba2a
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-08-04 22:28:03 -0500

    Update games.js

--- scripts/inject/games.js
+++ scripts/inject/games.js
@@ -1,36 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('json/games.json')
-        .then(response => response.json())
-        .then(games => {
-            const all_games = document.getElementById('all');
-            games.forEach(game => {
-                const game_button = document.createElement('div');
-                game_button.className = 'game';
-
-                const game_logo = document.createElement('img');
-                game_logo.className = 'g-icon';
-                game_logo.src = game.logo;
-                game_logo.alt = game.title;
-
-                const meta = document.createElement('div');
-                meta.className = 'meta';
-
-                const game_title = document.createElement('div');
-                game_title.className = 'title';
-                game_title.textContent = game.title;
-
-                const creator = document.createElement('div');
-                creator.className = 'creator';
-                creator.textContent = `By: ${game.creator}`;
-
-                meta.appendChild(game_title);
-                meta.appendChild(creator);
-
-                game_button.appendChild(game_logo);
-                game_button.appendChild(meta);
-
-                all_games.appendChild(game_button);
-            });
-        })
-        .catch(error => console.error('Err:', error));
+document.addEventListener('DOMContentLoaded', function() {
+    gamegen();
 });
+
+function gamegen() {
+    fetch('/json/games.json')
+    .then(response => response.json())
+    .then(data => {
+        const allsec = document.getElementById('all');
+        const injected = new Set();
+        data.forEach(game => {
+            if (game.tags && game.tags.length > 0) {
+                game.tags.forEach(tag => {
+                    if (categoryDiv && !injected.has(game.title)) {
+                        const gamebtn = document.createElement('div');
+                        gamebtn.classList.add('game');
+                        gamebtn.innerHTML = `
+                        <div class="game">
+                            <img class="g-icon" src="${game.logo}" alt="${game.title}">
+                            <div class="meta">
+                                <div class="title">${game.title}</div>
+                                <div class="creator">By: ${game.creator}</div>
+                            </div>
+                        </div>
+                        `;
+                        allsec.appendChild(gamebtn);
+                        injected.add(game.title);
+                    }
+                });
+            }
+        });
+    })
+    .catch(error => {
+        console.error('404:', error);
+    });
+}
