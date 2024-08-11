19d619bf40b196f0af6e9129bc44369f62b23d62
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-08-11 13:24:55 -0500

    Update footer.js

--- scripts/inject/footer.js
+++ scripts/inject/footer.js
@@ -9,7 +9,7 @@ document.write(`
         </div>
         <div class="footer-2">
             <p class="footer-title">Developers</p>
-            <a class="footer-a" href="https://x88.cybriatech.dev" target="_blank">MyBooty165 (X-88)</a>
+            <a class="footer-a" href="https://github.com/MyBooty165" target="_blank">MyBooty165 (X-88)</a>
         </div>
         <div class="footer-3">
             <p class="footer-title">Community</p>
