b57863a28bdef923a7dd263dbccddcb0abf2d3e9
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-08-12 13:57:18 -0500

    Update header.js

--- scripts/inject/header.js
+++ scripts/inject/header.js
@@ -7,7 +7,8 @@ document.write(`
             <a class="header-link" href="/proxies.html">Proxies</a>
             <a class="header-link" href="/bookmarklets.html">Bookmarklets</a>
             <a class="header-link" href="/chat.html">Chat</a>
-            <a class="header-profile" style="cursor: pointer;"><img src="/images/chat/placeholder.png" class="profile-picture" alt="Profile"></a>
+            <a class="header-link" href="/settings.html">Settings</a>
+            // <a class="header-profile" style="cursor: pointer;"><img src="/images/chat/placeholder.png" class="profile-picture" alt="Profile"></a>
         </nav>
     </header>
 `);
