b65b7a2efd4e1e898be3bd26eb80130f3b29f5e3
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-08-24 17:51:05 -0500

    Create emergency.js

---
+++ scripts/main/emergency.js
@@ -0,0 +1,6 @@
+document.addEventListener("keydown", function (event) {
+  if (event.key === "`") {
+    history.replaceState(null, "", "https://www.google.com");
+    window.location.href="https://www.google.com";
+  }
+});
