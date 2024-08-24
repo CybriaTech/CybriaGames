78c07a58f33d28cc84badf281608d8dde88ae63d
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-08-24 17:56:03 -0500

    Update emergency.js

--- scripts/main/emergency.js
+++ scripts/main/emergency.js
@@ -1,6 +1,5 @@
 document.addEventListener("keydown", function (event) {
   if (event.key === "`") {
-    history.replaceState(null, "", "https://www.google.com");
     window.location.href = "https://www.google.com";
   }
 });
