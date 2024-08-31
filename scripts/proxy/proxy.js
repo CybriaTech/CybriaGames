7f82d5ecee312b0d8cfc0ea9c960bffb0f0dc87a
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-08-31 13:24:05 -0500

    Create proxy.js

---
+++ scripts/proxy/proxy.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const console = document.getElementById("proxy-console");
+    const button = document.querySelectorAll(".proxy-button");
+
+    fetch('/json/proxy.json')
+        .then(response => response.json())
+        .then(data => {
+            button.forEach(button => {
+                button.addEventListener("click", function() {
+                    const proxyid = this.id;
+                    const links = data[0][proxyid];
+                    if (links) {
+                        console.innerHTML = '';
+                        links.forEach(link => {
+                            const injectlinks = document.createElement('div');
+                            injectlinks.textContent = link;
+                            console.appendChild(injectlinks);
+                        });
+                    }
+                });
+            });
+        })
+        .catch(error => {
+            console.error('JSON is unreachable:', error);
+        });
+});
