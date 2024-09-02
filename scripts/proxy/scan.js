b773c73c54a78fb234fd4578eb4f10080661b8b4
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-09-02 14:36:02 -0500

    Create scan.js

---
+++ scripts/proxy/scan.js
@@ -0,0 +1,7 @@
+function filter() {
+    const console = document.getElementById("proxy-console");
+    const corsproxy = [
+        "https://cors-anywhere.herokuapp.com/",
+        "https://cors.timmytamle569.workers.dev/"
+    ];
+}
