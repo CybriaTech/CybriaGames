cf9c030584876a39a77d7e5f2b2143c770500d34
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-09-07 14:05:31 -0500

    Create cheats.js

---
+++ scripts/inject/cheats.js
@@ -0,0 +1,33 @@
+async function inject() {
+    try {
+        const response = await fetch('/json/hacks.json');
+        const cheats = await response.json();
+
+        cheats.sort((a, b) => a.title.localeCompare(b.title));
+
+        const cheatarea = document.getElementById('cheats-area');
+
+        cheatarea.innerHTML = '';
+
+        cheats.forEach(cheat => {
+            const cheatoption = document.createElement('div');
+            cheatoption.classList.add('cheat');
+
+            const titlep = document.createElement('p');
+            titlep.classList.add('cheat-title');
+            titlep.textContent = cheat.name;
+
+            const creatorp = document.createElement('p');
+            creatorp.classList.add('cheat-creator');
+            creatorp.textContent = `By: ${cheat.author}`;
+
+            cheatoption.appendChild(titlep);
+            cheatoption.appendChild(creatorp);
+
+            cheatoption.appendChild(img);
+            cheatoption.appendChild(cheatoption);
+
+            cheatarea.appendChild(cheatoption);
+        });
+
+inject();
