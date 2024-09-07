d97955d5b496056b0421c85808cd5521433689ad
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-09-07 13:38:06 -0500

    Update header.js

--- scripts/inject/header.js
+++ scripts/inject/header.js
@@ -5,7 +5,7 @@ document.write(`
             <a class="header-link" href="/">Home</a>
             <a class="header-link" href="/games">Games</a>
             <a class="header-link" href="/proxies.html">Proxies</a>
-            <a class="header-link" href="/bookmarklets.html">Bookmarklets</a>
+            <a class="header-link" href="/cheats.html">Cheats</a>
             <a class="header-link" href="/chat.html">Chat</a>
             <a class="header-link" href="/settings.html">Settings</a>
             <!-- <a class="header-profile" style="cursor: pointer;"><img src="/images/chat/placeholder.png" class="profile-picture" alt="Profile"></a> -->
