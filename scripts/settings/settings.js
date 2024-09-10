16bab5cb56a2222fa4005ded78567c07eb4cf7f0
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-09-10 17:47:59 -0500

    Update settings.js

--- scripts/settings/settings.js
+++ scripts/settings/settings.js
@@ -39,7 +39,3 @@ function redircloak(title, iconUrl) {
 function ab() {
     redircloak("about:blank", "/images/cloak/about-blank.png");
 }
-
-function ntc() {
-    redircloak("New Tab", "/images/cloak/favi/new-tab-page.png");
-}
