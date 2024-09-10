1c2e72118aa27debe180ccfcb22f456c939ffc83
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-09-10 06:24:08 -0500

    Create settings.js

---
+++ scripts/settings/settings.js
@@ -0,0 +1,37 @@
+function ab() {
+    let inFrame;
+    
+    try {
+        inFrame = window !== top;
+    } catch (e) {
+        inFrame = true;
+    }
+
+    if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+        const popup = window.open("about:blank", "_blank");
+
+        if (!popup || popup.closed) {
+        } else {
+            const doc = popup.document;
+
+            doc.title = title;
+
+            const link = doc.createElement("link");
+            link.rel = "icon";
+            link.href = iconUrl;
+            doc.head.appendChild(link);
+
+            const iframe = doc.createElement("iframe");
+            const style = iframe.style;
+            iframe.src = location.href;
+            style.position = "fixed";
+            style.top = style.bottom = style.left = style.right = 0;
+            style.border = style.outline = "none";
+            style.width = style.height = "100%";
+
+            doc.body.appendChild(iframe);
+
+            location.replace("about:blank");
+        }
+    }
+}
