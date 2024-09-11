169d57a90f4012d77c28b4043d9b76321431d35e
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-09-10 21:08:52 -0500

    Update settings.js

--- scripts/settings/settings.js
+++ scripts/settings/settings.js
@@ -135,7 +135,7 @@ function def() {
   var link = document.querySelector("link[rel='icon']") || document.createElement("link");
   link.type = "image/x-icon";
   link.rel = "icon";
-  link.href = "";
+  link.href = "/images/cybriagames/favicon-circle.png";
 
   if (!document.querySelector("link[rel='icon']")) {
       document.head.appendChild(link);
