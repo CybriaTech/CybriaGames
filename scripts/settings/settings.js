96e9830a90ede18c5d9baaff8d4ea729b85292b5
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-09-13 07:05:03 -0500

    Update settings.js

--- scripts/settings/settings.js
+++ scripts/settings/settings.js
@@ -278,3 +278,30 @@ function lightmode() {
     document.querySelector('.header').style.backgroundColor = "#ffffff";
     document.querySelector('.footer').style.backgroundColor = "#ffffff";
 }
+
+function intersections(entries, observer) {
+    entries.forEach(entry => {
+        const certainbtn = document.querySelector(`.sidebar-btn[data-target="#${entry.target.id}"]`);
+
+        if (entry.isIntersecting) {
+            document.querySelectorAll('.sidebar-btn').forEach(btn => btn.classList.remove('active'));
+
+            if (certainbtn) {
+                certainbtn.classList.add('active');
+            }
+        }
+    });
+}
+
+const observeroption = {
+    root: null,
+    threshold: 0.5
+};
+
+const observer = new IntersectionObserver(intersettingspages, observeroption);
+
+const settingspages = document.querySelectorAll('#tabcloak, #emergencykeys, #themes');
+settingspages.forEach(settingspages => {
+    observer.observe(settingspages);
+});
+
