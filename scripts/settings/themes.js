0498d870b2cc44b6086cbd8595231086e425f667
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-09-13 22:16:59 -0500

    Update themes.js

--- scripts/settings/themes.js
+++ scripts/settings/themes.js
@@ -17,6 +17,8 @@ function lightmode() {
   var logo = document.querySelector('.header-img');
   var homelogo = document.querySelector('.title-img');
   var currenttheme = body.dataset.theme || 'light';
+  var header = document.querySelector('header');
+  var footer = document.querySelector('footer');
     
   body.dataset.theme = 'light';
   logo.src = '/images/cybriagames/logo-name2half-lightmode.png';
@@ -34,6 +36,8 @@ function darkmode() {
   var logo = document.querySelector('.header-img');
   var homelogo = document.querySelector('.title-img');
   var currenttheme = body.dataset.theme || 'dark';
+  var header = document.querySelector('header');
+  var footer = document.querySelector('footer');
     
   body.dataset.theme = 'dark';
   logo.src = '/images/cybriagames/logo-name2half.png';
