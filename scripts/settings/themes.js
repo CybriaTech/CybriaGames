77f57acff817f6a86572fff2ba38ba294f5e6f64
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-09-17 21:42:52 -0500

    Update themes.js

--- scripts/settings/themes.js
+++ scripts/settings/themes.js
@@ -10,6 +10,8 @@ document.addEventListener('DOMContentLoaded', function() {
 function applyls() {
     const savedtheme = localStorage.getItem('theme') || 'Default';
     settheme(savedtheme);
+    const savedparticle = localStorage.getItem('particles') || 'Default';
+    settheme(savedparticle);
 }
 
 function settheme(theme) {
@@ -196,4 +198,42 @@ function clearparticles() {
         particlesarea.innerHTML = '';
     }
 }
+
+function setparticles(particle) {
+    body.dataset.particles = particles;
+
+    clearparticles();
+
+    if (particles === 'None') {
+        localStorage.setItem('particles');
+        clearparticles();
+    } else if (particles === 'Graphs') {
+        clearparticles();
+   particlesJS('particles-js', {
+     particles: {
+       number: { value: 80, density: { enable: true, value_area: 800 } },
+       color: { value: '#ffffff' },
+       shape: { type: 'circle', stroke: { width: 0, color: '#000000' }, polygon: { nb_sides: 5 }, image: { src: 'img/github.svg', width: 100, height: 100 } },
+       opacity: { value: 0.5, random: false, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
+       size: { value: 3, random: true, anim: { enable: false, speed: 40, size_min: 0.1, sync: false } },
+       line_linked: { enable: true, distance: 150, color: '#ffffff', opacity: 0.4, width: 1 },
+       move: { enable: true, speed: 6, direction: 'none', random: false, straight: false, out_mode: 'bounce', bounce: false, attract: { enable: false, rotateX: 600, rotateY: 1200 } }
+     },
+     interactivity: {
+       detect_on: 'canvas',
+       events: { onhover: { enable: true, mode: 'repulse' }, onclick: { enable: true, mode: 'push' }, resize: true },
+       modes: { grab: { distance: 400, line_linked: { opacity: 1 } }, bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 }, repulse: { distance: 200, duration: 0.4 }, push: { particles_nb: 4 }, remove: { particles_nb: 2 } }
+     },
+     retina_detect: true
+   });
+    } else if (particles === 'Bubble') {
+        
+    } else if (particles === 'Snow') {
+        
+    }
+
+    localStorage.setItem('particles', particles);
+    document.getElementById("particles").value = particles;
+}
+
 
