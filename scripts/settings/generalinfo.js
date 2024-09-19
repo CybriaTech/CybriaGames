2a4e5122f632cd6d7d5ede65cc4b8cf06bee2af5
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-09-19 18:46:30 -0500

    Update generalinfo.js

--- scripts/settings/generalinfo.js
+++ scripts/settings/generalinfo.js
@@ -7,7 +7,7 @@ async function generalinfo() {
         document.getElementById('infocreated').textContent = `Created: ${configdata[0].Created}`;
         document.getElementById('infover').textContent = `Version: ${configdata[0].Version}`;
         document.getElementById('infocreator').textContent = `Creator: ${configdata[0].Creator}`;
-        const pageurl = window.location.origin;
+        const pageurl = window.location.origin + '/';
 
         const officiallinks = configdata[0].officiallinks;
 
