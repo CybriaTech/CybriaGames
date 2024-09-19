16d404de09fd00a366a43f912fd78b5271245ff1
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-09-19 18:34:51 -0500

    Update generalinfo.js

--- scripts/settings/generalinfo.js
+++ scripts/settings/generalinfo.js
@@ -7,8 +7,12 @@ async function generalinfo() {
         document.getElementById('infocreated').textContent = `Created: ${configdata[0].Created}`;
         document.getElementById('infover').textContent = `Version: ${configdata[0].Version}`;
         document.getElementById('infocreator').textContent = `Creator: ${configdata[0].Creator}`;
-        const official = configdata[0].Creator === 'CybriaTech';
-        document.getElementById('infoofficial').textContent = Official: ${official};
+        const pageurl = window.location.href;
+
+        const officiallinks = configdata[0].officiallinks;
+
+        const official = pageurl === officiallinks;
+        document.getElementById('infoofficial').textContent = `Official: ${official}`;
 
         const gamejson = await fetch(configdata[0].Games);
         const gamedata = await gamejson.json();
